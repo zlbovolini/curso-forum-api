ed3d188a773d95e262b7bd466d1a587bfdbfd3b8
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-07-20 16:23:48 -0300

    Primeiro commit

---
+++ src/main/java/com/github/lbovolini/forum/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.github.lbovolini.forum.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
---
+++ src/main/java/com/github/lbovolini/forum/modelo/StatusTopico.java
@@ -0,0 +1,10 @@
+package com.github.lbovolini.forum.modelo;
+
+public enum StatusTopico {
+	
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+
+}
