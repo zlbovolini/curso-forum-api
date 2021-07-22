f9174e4f4a1868ee9521a690609373dfd4534acd
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-07-22 06:24:16 -0300

    Adicionado Swagger

--- src/main/java/com/github/lbovolini/forum/ForumApplication.java
+++ src/main/java/com/github/lbovolini/forum/ForumApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableCaching
 @EnableSpringDataWebSupport
 @SpringBootApplication
